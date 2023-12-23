fee693baf2804939b8009ca4e761f0a35c1536a8
Abdullahtahirrr <abdullah.tahirr7@gmail.com>
2023-12-23 23:09:17 +0500

    Added Test Cases for ticketreport.java

---
+++ src/out/production/airlaib/test/TicketReportTest.java
@@ -0,0 +1,132 @@
+package out.production.airlaib.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import out.production.airlaib.admindomain;
+import out.production.airlaib.ticketreport;
+
+/**
+ * Javadoc for TicketReportTest class.
+ *
+ * JUnit test class for the {@code ticketreport} class.
+ * @author Abdullah Tahir 
+ * @version 1.0
+ * @since 2023-11-21
+ */
+public class TicketReportTest {
+
+    // Fields for mock objects
+	 private Connection mockConnection;
+	   private PreparedStatement mockPreparedStatement;
+	    private ResultSet mockResultSet;
+
+	    /**
+	     * Set up the mock database connection, statement, and result set.
+	     * 
+	     * @throws SQLException If a SQL exception occurs during setup.
+	     */ 
+	    @Before
+	    public void setUp() throws SQLException {
+	        // Mock the database connection, statement, and result set
+	        mockConnection = mock(Connection.class);
+	        mockPreparedStatement = mock(PreparedStatement.class);
+	        mockResultSet = mock(ResultSet.class);
+
+	        when(mockConnection.prepareStatement(any(String.class))).thenReturn(mockPreparedStatement);
+	        when(mockPreparedStatement.executeQuery()).thenReturn(mockResultSet);
+
+	        // Set up the mock result set to return a user when queried
+	        when(mockResultSet.next()).thenReturn(true);
+	    }
+
+
+	    /**
+	     * Close the mock connection after each test.
+	     * 
+	     * @throws SQLException If a SQL exception occurs during teardown.
+	     */
+	    @After
+	    public void tearDown() throws SQLException {
+	        // Close the mock connection after each test
+	        if (mockConnection != null) {
+	            mockConnection.close();
+	        }
+	    }
+
+	    @Test
+	    public void testLoadData() {
+	        ticketreport report = new ticketreport();
+	        
+	        
+	        // Call the method to load data
+	        report.LoadData();
+	        
+	        // Verify that the table has data
+	        DefaultTableModel model = (DefaultTableModel) report.ticketsTable.getModel();
+	        assertTrue(model.getRowCount() > 0);
+	        assertTrue(model.getColumnCount() > 0);
+	    }
+	    @Test
+	    public void testConfirmButtonActionPerformed() throws SQLException  {
+	        ticketreport report = new ticketreport();
+	        report.setConnection(mockConnection);
+	        
+	        // Create a mock PreparedStatement
+	        PreparedStatement mockPreparedStatement = mock(PreparedStatement.class);
+	        when(mockConnection.prepareStatement(any(String.class))).thenReturn(mockPreparedStatement);
+	        
+	        // Create a mock ResultSet
+	        ResultSet mockResultSet = mock(ResultSet.class);
+	        when(mockPreparedStatement.executeUpdate()).thenReturn(1); // Update successful
+	        
+	        // Set a selected row in the table
+	        report.ticketsTable.addRowSelectionInterval(0, 0);
+	        
+	        // Call the method to confirm the ticket
+	        String result = report.confirmButtonActionPerformed(null);
+	        
+	        // Verify that the confirmation status is updated successfully
+	        assertEquals("Successful", result);
+	    }
+
+	    
+	    @Test
+	    public void testBackBtnAction() {
+	        ticketreport report = new ticketreport();
+	        
+	        // Create a mock admin domain window
+	        admindomain mockAdminDomain = mock(admindomain.class);
+	        report.setAdminDomainObj(mockAdminDomain);
+	        
+	        // Call the method to go back
+	        report.backBtnAction(null);
+	        
+	        // Verify that the current window is closed and the admin domain window is opened
+	        assertTrue(report.getAdminDomainObj().isVisible());
+	    }
+
+
+    
+
+}
+
