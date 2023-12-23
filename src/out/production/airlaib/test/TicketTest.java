403c0410f5ec03934b76ebbfc315ba5d445b891e
Marriam-Naeem <marriamnaeem435@gmail.com>
2023-12-23 17:09:15 +0500

    Modifed Tests for ticket.java

--- src/out/production/airlaib/test/TicketTest.java
+++ src/out/production/airlaib/test/TicketTest.java
@@ -9,21 +9,18 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.awt.event.MouseEvent;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.JComboBox;
 
 public class TicketTest {
 
@@ -132,5 +129,55 @@ public class TicketTest {
         // Assert
         // Check if the admin domain window is opened
         assertFalse(ticket.getBillObj().isVisible());
+    }
+    
+    @Test
+    public void testupdateDestinationDropdown() {
+    	ticket ticket = new ticket();
+    	ticket.getTxtsource().setSelectedItem("Pakistan");
+    	
+    	//act
+    	ticket.updateDestinationDropdown();
+    	
+    	//assert
+    	assertEquals(5, ((ticket.getTxtdepart()).getItemCount()));
+    	assertFalse(itemExistsInComboBox(ticket.getTxtdepart(), "Pakistan"));
+
+        // Check if the non-selected sources are present in the destination dropdown
+        assertTrue(itemExistsInComboBox(ticket.getTxtdepart(), "Srilanka"));
+        assertTrue(itemExistsInComboBox(ticket.getTxtdepart(), "Uk"));
+        assertTrue(itemExistsInComboBox(ticket.getTxtdepart(), "Usa"));
+        assertTrue(itemExistsInComboBox(ticket.getTxtdepart(), "Canada"));
+        assertTrue(itemExistsInComboBox(ticket.getTxtdepart(), "China"));
+    }
+    
+    // Helper method to check if an item exists in a JComboBox
+    private boolean itemExistsInComboBox(JComboBox<String> comboBox, String item) {
+        for (int i = 0; i < comboBox.getItemCount(); i++) {
+            if (comboBox.getItemAt(i).equals(item)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
+    @Test
+    public void testDisablePastDates() throws ParseException {
+        
+        ticket ticket = new ticket();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = "2023-12-13";
+        java.util.Date utilDate = dateFormat.parse(dateString);
+        // Convert java.util.Date to java.sql.Date
+        java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+        ticket.getTxtdate().setDate(sqlDate);
+
+        // Call the method to be tested
+        ticket.disablePastDates(ticket.getTxtdate());
+
+        // Verify that the date was reset to the current date
+        assertEquals(new Date(), ticket.getTxtdate().getDate());
     }
 }
