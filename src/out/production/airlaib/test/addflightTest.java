92964c2334ef437e278669e145281abd9d21c4f7
Imaan <imaanibrar86@gmail.com>
2023-12-27 22:04:31 +0500

    Documented Testing Strategy

--- src/out/production/airlaib/test/addflightTest.java
+++ src/out/production/airlaib/test/addflightTest.java
@@ -1,31 +1,60 @@
-/**
- * JUnit test class for the addflight class.
- * Author: Imaan Ibrar
- */
 package out.production.airlaib.test;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import java.awt.event.KeyEvent;
 
+
 import out.production.airlaib.addflight;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 /**
- * Test class for the addflight class.
+ * JUnit test class for the addflight class.
+ * Author: Imaan Ibrar
+ *
+ * Testing strategy:
+ * - Partition the inputs for testSuccessfulFlightAddition:
+ *   - Set valid values for all input fields.
+ *   - Verify that the "Add" button is disabled after the action.
+ *
+ * - Partition the inputs for testUnsuccessfulFlightAddition:
+ *   - Set invalid values for one or more input fields (e.g., non-numeric charge).
+ *   - Verify that the "Add" button remains enabled after the action.
+ *
+ * - Test the back button action:
+ *   - Call the back button action.
+ *   - Verify that the adminDomainObj window is visible after the action.
+ *
+ * - Test key released events:
+ *   - Test the key released event for departure time:
+ *     - Set a valid departure time.
+ *     - Verify that the warning label for departure time is null after the action.
+ *   - Test the key released event for arrival time:
+ *     - Set a valid arrival time.
+ *     - Verify that the warning label for arrival time is null after the action.
+ *   - Test the key released event for flight charge:
+ *     - Set a valid flight charge.
+ *     - Verify that the warning label for flight charge is null after the action.
+ *
+ * - Additional considerations:
+ *   - Mock the behavior of the database connection, statement, and result set for various scenarios.
+ *   - Test with SQL exceptions to ensure proper exception handling.
  */
 public class addflightTest {
 
@@ -33,8 +62,7 @@ public class addflightTest {
     private addflight addFlight;
     private PreparedStatement mockPreparedStatement;
     private ResultSet mockResultSet;
-
-    /**
+ /**
      * Setup method to initialize mocks and set up the class for testing.
      *
      * @throws SQLException if a SQL exception occurs
@@ -56,8 +84,7 @@ public class addflightTest {
         addFlight = new addflight();
         addFlight.setConnection(mockConnection);
     }
-
-    /**
+/**
      * Tear down method to close the mock connection after each test.
      *
      * @throws SQLException if a SQL exception occurs
@@ -70,48 +97,96 @@ public class addflightTest {
         }
     }
 
-    /**
+ /**
      * Test for successful flight addition.
      *
      * @throws SQLException if a SQL exception occurs
      */
     @Test
     public void testSuccessfulFlightAddition() throws SQLException {
-        // ... (unchanged code)
-    }
+    	addFlight.setConnection(mockConnection);
 
-    /**
+        // Set initial values using setText method
+        addFlight.getFlightIdText().setText("FO037");
+        addFlight.getFlightNameText().setText("Test Flight");
+        addFlight.getSourceText().setSelectedItem("Pakistan");
+        addFlight.getDepartureText().setSelectedItem("Uk");
+        addFlight.getDepartureTimeText().setText("12:00");
+        addFlight.getArrivalTimeText().setText("14:00");
+        addFlight.getFlightChargeText().setText("50000");
+        
+
+        // Action
+        addFlight.AddBtnAction(null);
+
+        // After the action, the button should be disabled
+        System.out.println("After Action: Is Button Enabled: " + addFlight.getAddBtn().isEnabled());
+        
+        // Check the state after the action
+        assertFalse("Button should be disabled after action", addFlight.getAddBtn().isEnabled());
+    }
+ /**
      * Test for unsuccessful flight addition.
      *
      * @throws SQLException if a SQL exception occurs
      */
     @Test
     public void testUnsuccessfulFlightAddition() throws SQLException {
-        // ... (unchanged code)
-    }
+    	addFlight.setConnection(mockConnection);
+    	 // Set initial values using setText method
+        addFlight.getFlightIdText().setText("FO038");
+        addFlight.getFlightNameText().setText("Test Flight");
+        addFlight.getSourceText().setSelectedItem("Pakistan");
+        addFlight.getDepartureText().setSelectedItem("Uk");
+        addFlight.getDepartureTimeText().setText("12:00");
+        addFlight.getArrivalTimeText().setText("14:00");
+        addFlight.getFlightChargeText().setText("abc");
 
+        addFlight.AddBtnAction(null);
+
+        assertTrue(addFlight.getAddBtn().isEnabled());
+    }
     /**
      * Test for back button action.
      */
     @Test
     public void testBackBtnAction() {
-        // ... (unchanged code)
-    }
+        // Action: Call the back button action
+        addFlight.backBtnAction(null);
 
-    /**
+        // After the action, check if the adminDomainObj window is visible
+        assertTrue("Admin Domain window should be visible after back button action", addFlight.getAdminDomainObj().isVisible());
+    }
+ /**
      * Test for departure time key released.
      */
+
     @Test
     public void testTxtdtimeKeyReleased() {
-        // ... (unchanged code)
-    }
+        // Set initial values
+        addFlight.getDepartureTimeText().setText("12:00");
 
-    /**
+        // Call your method with a mock key event
+        KeyEvent mockKeyEvent = mock(KeyEvent.class);
+        addFlight.txtdtimeKeyReleased(mockKeyEvent);
+
+        // Assert any relevant behavior or state changes
+        assertNull(addFlight.getWarningDepartureTime().getText()); // Assuming you have a method to get the warning label
+    }
+/**
      * Test for arrival time key released.
      */
     @Test
     public void testTxtarrtimeKeyReleased() {
-        // ... (unchanged code)
+        // Set initial values using setText method
+        addFlight.getArrivalTimeText().setText("14:00");
+
+        // Action: Call your method with a mock key event
+        KeyEvent mockKeyEvent = mock(KeyEvent.class);
+        addFlight.txtarrtimeKeyReleased(mockKeyEvent);
+
+        // After the action, check the state
+        assertNull("Warning label for arrival time should be null after action", addFlight.getWarningArrivalTime().getText());
     }
 
     /**
@@ -119,7 +194,15 @@ public class addflightTest {
      */
     @Test
     public void testTxtflightchargeKeyReleased() {
-        // ... (unchanged code)
+        // Set initial values using setText method
+        addFlight.getFlightChargeText().setText("50000");
+
+        // Action: Call your method with a mock key event
+        KeyEvent mockKeyEvent = mock(KeyEvent.class);
+        addFlight.txtflightchargeKeyReleased(mockKeyEvent);
+
+        // After the action, check the state
+        assertNull("Warning label for flight charge should be null after action", addFlight.getWarningFlightCharge().getText());
     }
 
 }
